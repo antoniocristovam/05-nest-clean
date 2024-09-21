d1c08fe298d6cf2384349041a59bf8951b2912c3
antoniocristovam <antoniocristovam@outlook.com.br>
2024-09-21 18:37:17 -0300

    feat: Adjust controller and module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,8 @@ import { CreateAccountController } from "./controllers/create-account.controller
 import { ConfigModule } from "@nestjs/config";
 import { envSchema } from "./env";
 import { AuthModule } from "./auth/auth.module";
-import { AuthenticateController } from "./controllers/authenticate-controller";
+import { AuthenticateController } from "./controllers/authenticate.controller";
+import { CreateQuestionsController } from "./controllers/create-questions.controllers";
 
 @Module({
   imports: [
@@ -14,7 +15,11 @@ import { AuthenticateController } from "./controllers/authenticate-controller";
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionsController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
